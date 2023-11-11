877a2854780fc1b203773e93df7cb336b73b2b89
saul mamani <luas0_1@yahoo.es>
2023-11-11 09:27:48 -0400

    Cambios en el Context

--- CITIproject/Context/MiContext.cs
+++ CITIproject/Context/MiContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CITIproject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CITIproject.Context
 {
@@ -7,5 +8,8 @@ namespace CITIproject.Context
         public MiContext() { 
             
         }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Ingeniero> Ingenieros { get; set; }
     }
 }
