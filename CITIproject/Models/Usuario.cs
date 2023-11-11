f068751c8a0f846c3f6188d152ce0f756da4c59a
saul mamani <luas0_1@yahoo.es>
2023-11-11 09:15:47 -0400

    Se anadio Identicador a la clase Ingeniero

--- CITIproject/Models/Usuario.cs
+++ CITIproject/Models/Usuario.cs
@@ -4,6 +4,7 @@ namespace CITIproject.Models
 {
     public class Usuario
     {
+        public int Id { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? NombreCompleto { get; set; }
